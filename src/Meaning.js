d9a99e4b21f86220af340cd1b76acc2123340a94
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-25 21:23:50 +0100

    synonyms are working

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-console.log(props.meaning);
+console.log(props.meaning.synonyms);
     return(
         <div className="Meaning">
+          <span className="mt-5">
+          <strong><em>{props.meaning.partOfSpeech}</em></strong></span>
+        
           {props.meaning.definitions.map(function(definition, index) {
             return (
                 <div key={index}>
                     <p>
-                    {definition.definition}
+                    <strong>Definition: </strong>{definition.definition}
                     <br />
-                    <p>Example of usage</p>
-                    <em>{definition.example}</em>
+                    <strong>Example of usage:</strong> <em>{definition.example}</em>
+                    <br />
+                    <strong>Synonyms:</strong> 
+                    <Synonyms synonyms={definition.synonyms} />
                     </p>
                 </div>
             );
