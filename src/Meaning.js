361e78c6e50521e22bed8d03b3af9a01c6bef885
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-25 14:57:10 +0100

    additional index removed

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,17 +4,13 @@ export default function Meaning(props) {
 console.log(props.meaning);
     return(
         <div className="Meaning">
-           <h3>{props.meaning.partOfSpeech}</h3>
-                <p>{props.meaning.definitions[0].definition}</p>
-                <p>Example of usage</p>
-                <p><em>{props.meaning.definitions[0].example}</em></p>
-
-           {props.meaning.definitions.map(function(definition, index) {
+          {props.meaning.definitions.map(function(definition, index) {
             return (
                 <div key={index}>
                     <p>
                     {definition.definition}
                     <br />
+                    <p>Example of usage</p>
                     <em>{definition.example}</em>
                     </p>
                 </div>
