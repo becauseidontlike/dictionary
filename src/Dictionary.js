b8c2a47b6967eb128b11d45694615a4eac050d69
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-26 17:27:28 +0100

    pics from pexels api included

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) {
 const [keyword, setKeyword] = useState(props.defaultKeyword);
 const [results, setResults] = useState(null);
 const [loaded, setLoaded] = useState(false);
+const [photos, setPhotos] = useState(null);
     
 function handleResponse(response) {
-console.log(response.data[0].meanings[0].definitions[0].definition)
 setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
 }
 
 function search() {
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 axios.get(apiUrl).then(handleResponse); 
+
+let pexelsApiKey ="fmKiQNIw53wLZE2sywuJV8HWwJwMTm0HOm8KFLmrDlulrmJSRQBPGxGf";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+
+let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse); 
 }
-
-
 
 function searchWord(event) {
 event.preventDefault();
@@ -47,6 +57,7 @@ if (loaded) {
         </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
         </div>
     )
 } else {
